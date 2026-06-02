4424585fa4a14feba31f2d41ea39141a8ed3378d
Liz Zhu liz.zhu@mail.utoronto.ca; MEng; Primary WS2 <zhuzih19@iMed-WS2.localdomain>
2026-06-02 19:52:17 -0400

    update DATA_ROOT for server path, N_LAYERS=12

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-DATA_ROOT = "/Volumes/csi/default/raw_data/csi-bench-dataset/csi-bench-dataset"
+DATA_ROOT = "/home/zhuzih19/raw_data/csi-bench-dataset/csi-bench-dataset"
 
 # Model
 IN_CHANNELS = 1
